c0466149fd2b7dc4d0f3700e36ba77685e5ea8b2
Sergd112 <sergio.gomezdamas@gmail.com>
2020-09-26 18:15:19 +0200

    update

--- backend/MeetacAPI/Meetac/models.py
+++ backend/MeetacAPI/Meetac/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class MeetacUser(User):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    tags=models.JSONField(blank=True)
-    policy= models.BooleanField(default=False)
-    birthday= models.DateField()
-    description=models.TextField(max_length=256)
-    profileimg= models.ImageField()
+    tags = models.JSONField(blank=True)
+    policy = models.BooleanField(default=False)
+    birthday = models.DateField()
+    description = models.TextField(max_length=256)
+    profileimg = models.ImageField()
+    selected = models.JSONField(blank=True)
+    matches = models.JSONField(blank=True)
+    banned = models.IntegerField(default=0)
+    reported = models.JSONField(blank=True)
+    viewed = models.JSONField(blank=True)
+    
 
+class Message(models.Model):
+    id_sender= models.ForeignKey(MeetacUser)
+    id_receiver= models.ForeignKey(MeetacUser)
+    type= models.CharField(max_length=32,default='text')
+    body=models.JSONField(blank=True)
+    datetime=models.DateTimeField(_, auto_now=True, auto_now_add=False)
+    recieved=models.BooleanField(default=False)
+    read=models.BooleanField(default=False)
+
+class Graph_history(models.Model):
+    date_time=models.DateTimeField(_, auto_now=True, auto_now_add=False)
+    graph= models.JSONField(blank=True)
--- backend/MeetacAPI/Meetac/views.py
+++ backend/MeetacAPI/Meetac/views.py
@@ -3,10 +3,15 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def apiOverview(request):
     api_urls={
         'List':'/list/',
         'Detail View':'/detail/<str:pk>',
+        'Graph':'/graph'
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def get_graph(request):
+    return Response({})
