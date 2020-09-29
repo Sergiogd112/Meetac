505726926619e5bc00e651be3eb2af994cd59da3
Sergd112 <sergio.gomezdamas@gmail.com>
2020-09-29 10:11:05 +0200

    Update

--- backend/MeetacAPI/Meetac/models.py
+++ backend/MeetacAPI/Meetac/models.py
@@ -4,23 +4,23 @@ from django.contrib.auth.models import User
 
 class MeetacUser(User):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    tags = models.JSONField(blank=True)
+    tags = models.TextField(blank=True)
     policy = models.BooleanField(default=False)
     birthday = models.DateField()
     description = models.TextField(max_length=256)
     profileimg = models.ImageField()
-    selected = models.JSONField(blank=True)
-    matches = models.JSONField(blank=True)
+    selected = models.TextField(blank=True)
+    matches = models.TextField(blank=True)
     banned = models.IntegerField(default=0)
-    reported = models.JSONField(blank=True)
-    viewed = models.JSONField(blank=True)
+    reported = models.TextField(blank=True)
+    viewed = models.TextField(blank=True)
 
 
 class Message(models.Model):
     id_sender = models.ForeignKey(MeetacUser)
     id_receiver = models.ForeignKey(MeetacUser)
     type = models.CharField(max_length=32, default='text')
-    body = models.JSONField(blank=True)
+    body = models.TextField(blank=True)
     datetime = models.DateTimeField(_, auto_now=True, auto_now_add=False)
     recieved = models.BooleanField(default=False)
     read = models.BooleanField(default=False)
@@ -28,4 +28,9 @@ class Message(models.Model):
 
 class Graph_history(models.Model):
     date_time = models.DateTimeField(_, auto_now=True, auto_now_add=False)
-    graph = models.JSONField(blank=True)
+    graph = models.TextField(blank=True)
+
+class Tag(models.Model):
+    name=models.CharField(max_length=32)
+    users= models.ManyToManyField(User)
+    count=models.IntegerField(default=0)
--- backend/MeetacAPI/Meetac/views.py
+++ backend/MeetacAPI/Meetac/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import MeetacUser, Tag, Graph_history, Message
+
 
 @api_view(['GET'])
 def apiOverview(request):
-    api_urls={
-        'List':'/list/',
-        'Detail View':'/detail/<str:pk>',
-        'Graph':'/graph'
+    api_urls = {
+        'List': '/list/',
+        'Detail View': '/detail/<str:pk>',
+        'Graph': '/graph',
+        'Tags List': '/tags',
+        'Detail Tag': '/tag/<str:pk>'
     }
     return Response(api_urls)
+
 
 @api_view(['GET'])
 def get_graph(request):
